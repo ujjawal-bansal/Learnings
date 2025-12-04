a3ee23df734bb8f26a99a37c6b8cbf4a6d385aa8
ujjawal-bansal <ujjawalbansal63@gmail.com>
2025-12-05 00:57:28 +0530

    Updated ReactJS Code folder with new learnings

--- ReactJS/1_Inception/Code/App.js
+++ ReactJS/1_Inception/Code/App.js
@@ -1,4 +1,5 @@
 // PARCEL:
+// Created a server for our react app
 // HMR- Hot Module Replacement- it will reload only the changed modules instead of reloading the entire app
 // this is done by parcel itself
 // this means that parcel will keep a track of all the files which you are updating
@@ -17,14 +18,26 @@
 // port Number
 // Consistient Hashing algorithm
 // Zero Config
+// Tree Shaking- removing unwanted code
 
 
 
+// Browserlist is used to specify which browsers you want to support. we write it in package.json file.
+// e.g. "browserslist": [
+//   "last 2 chrome versions",
+// ]
+// this means that we want to support last 2 versions of all browsers, browsers which are not dead and browsers which have more than 0.2% market share.
+
+// npm init gives you a package.json file
+// npm init -y gives you a package.json file with default values
+
+// npm i -D parcel installs parcel as a dev dependency
+// npx parcel index.html (to run the app)
 
 
 import React from "react";
 import ReactDOM from "react-dom/client";
-// we have wrotes /client because from react 18 onwards we have to write like this
+// we have wrote '/client' because from react 18 onwards we have to write like this
 // importing react and react-dom libraries
 const heading = React.createElement(
   "h1",
@@ -37,16 +50,18 @@ const heading = React.createElement(
   },
   "Heading 1"
 );
-
 // this is same as <h1 id="title">Heading 1</h1>
+
+// React.createElement => Object => HTML(DOM)
 
 const heading2 = React.createElement(
   "h2",
   {
     id: "title",
     key: "h2",
-    // key is used when we have multiple similar elements
-    // react uses key to differentiate between them
+    //* key is used when we have multiple similar elements
+    //* react uses key to differentiate between them
+    // read reconciliation in react docs for more info
   },
   "Heading 2"
 );
@@ -58,7 +73,17 @@ const container = React.createElement(
   },
   [heading, heading2]
   // children can be an array also
+  // whenever there is an array, we have to provide key prop to each element 
 );
+
+// Generally, we don't use React.createElement method directly
+// We use something called JSX
+// Suppose I have to build a big structure with many nested elements
+// It will be very tedious to write React.createElement method again and again
+// So, we use JSX which is a syntax extension to javascript
+// It is not necessary to use JSX but it is recommended to use it
+// It makes our code easier to write and understand
+// JSX gets converted to React.createElement method calls by babel
 
 console.log(heading);
 
--- ReactJS/1_Inception/Code/README.md
+++ ReactJS/1_Inception/Code/README.md
@@ -1,4 +1,22 @@
 # Learnings
 
-# anything which we can generate on server can be put inside  .gitignor
+# anything which we can generate on server can be put inside  .gitignore
 # we are supposed not to push it
+
+
+# json important points:
+ "scripts": {
+    "start": "parcel index.html", 
+    // by writing this command we can start the server using 'npm run start' or 'npx start' in terminal 
+    // instead of writing 'npx parcel index.html'
+    // we didn't write npx because it is just exactly npm but but it is executing a package without really downloading it.
+    // npm run = npx
+
+    // you can also use npm start intead of npm run start as both are the same things.
+    
+    "build": "parcel build index.html",
+
+  },
+
+# Babel ek JavaScript transpiler hai jo modern code ko old browser compatible JavaScript me convert karta hai.
+babel-plugin-transform-remove-console is used to remove console logs.
